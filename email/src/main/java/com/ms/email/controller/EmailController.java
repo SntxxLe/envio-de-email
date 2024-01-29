30d3256ccea8267eb6de5c9f565c8e173e4cc6da
Maria Helena <maria_helena.ss@outlook.com>
2024-01-29 19:28:26 -0300

    Criando o serviço de envio Post

--- email/src/main/java/com/ms/email/controller/EmailController.java
+++ email/src/main/java/com/ms/email/controller/EmailController.java
@@ -1,7 +1,15 @@
 package com.ms.email.controller;
 
+import com.ms.email.dto.EmailDTO;
+import com.ms.email.model.EmailModel;
 import com.ms.email.service.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +17,12 @@ public class EmailController {
 
     @Autowired
     EmailService emailService;
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDTO emailDTO) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDTO, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
 }
